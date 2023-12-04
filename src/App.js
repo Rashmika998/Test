07451dd6ef8cbaa9f7f3e92dfdd79e2de80d9e1a
Rashmika99x <rashmikas@99x.io>
2023-12-04 15:49:15 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import "./App.css";
-import withUpperCase from './components/HOC/UpperCase';
-import DisplayText from './components/HOC/DisplayText';
 
 function App() {
   const title = "Hello React";
-  const DisplayTextWithUpperCase = withUpperCase(DisplayText);
 
   return (
     <div className="App">
-      <DisplayTextWithUpperCase text="Hello, HOC!" />
+      {title}
     </div>
   );
 }
--- src/components/HOC/DisplayText.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DisplayText = ({ modifiedText }) => {
-  return <p>Modified Text: {modifiedText}</p>;
-};
-
-export default DisplayText;
